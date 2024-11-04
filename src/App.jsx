9da0526ae0f8c740dd51e505184a94362e3454e9
gauravsondhiya <gauravsondhiya1503@gmail.com>
2024-11-04 23:08:50 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,68 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+  let buttons = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+    "/",
+    "*",
+    "-",
+    "+",
+    "=",
+    "CL"
+  ];
 
-  let [input,setinput] = useState('')
-  let [output,setoutput] = useState()
-
-let value=(i)=>{
-  setinput(input+i)
-}
-let final=()=>{
-  console.log(typeof(input))
-}
-
+  let [output, setoutput] = useState("");
+  let [shows, setshows] = useState("");
+  let inputvalues = (i) => {
+    setoutput(output + i);
+  };
+  let show = () => {
+   try{
+    setshows(eval(output));
+   } 
+   catch{
+    setshows("error")
+   }
+  };
+  let clear=()=>{
+    setoutput(" ")
+    setshows(" ")
+  }
   return (
     <div>
-      <button className='border border-black w-[50px] h-[50px]' onClick={()=>value(1)}>1</button>
-      <button className='border border-black w-[50px] h-[50px]' onClick={()=>value(2)}>2</button>
-      <button className='border border-black w-[50px] h-[50px]' onClick={()=>value('+')}>+</button>
-      <button className='border border-black w-[50px] h-[50px]' onClick={final}>=</button>
-      <h1>{input}</h1>
-      {/* <h1>{output}</h1> */}
+      <div className=" border-black h-[500px] justify-center">
+        <div className="border border-black w-[250px] h-[80px] m-auto mt-14 text-xl">
+        <h1>{output}</h1>
+        <h1>{shows}</h1>
+        </div>
+        
+        <div className="grid  grid-cols-4  border border-red-700 w-[250px] h-[280px] m-auto ">
+         
+          {buttons.map((btn, i) => (
+            <button
+              className="bg-blue-500 font-bold border-black h-[60px] w-[60px] gap-2"
+              key={i}
+              onClick={() => ( btn=="="?show() :btn=="CL"? clear(): inputvalues(btn))} >
+              {btn}
+            </button>
+          ))}
+        </div>
+      </div>
+    
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
 
-
-// import React, { useState } from 'react';
-
-// function App() {
-//   const [input, setInput] = useState("");
-//   const [result, setResult] = useState("");
-
-//   const handleClick = (value) => {
-//     setInput(input + value);
-//   };
-
-//   const clearInput = () => {
-//     setInput("");
-//     setResult("");
-//   };
-
-//   const calculate = () => {
-//     try {
-//       setResult(eval(input));
-//     } catch {
-//       setResult("Error");
-//     }
-//   };
-
-//   return (
-//     
-//         <input
-//           type="text"
-//           value={input}
-//           readOnly
-//         />
-//         <div >{result}</div>
-//         <div >
-//           {["1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "C", "0", "=", "/"].map((item) => (
-//             <button
-//               key={item}
-//               onClick={() => (item === "=" ? calculate() : item === "C" ? clearInput() : handleClick(item))}>
-//               {item}
-//             </button>
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
